73a856a84a722d9ce7d852039d958087900ea6df
BarasaIan11 <ebarasa845@gmail.com>
2025-08-20 23:30:43 +0300

    job controller update

--- server/controllers/jobController.js
+++ server/controllers/jobController.js
@@ -1,44 +1,87 @@
-import Job from "../models/Job.js"
-
-
-
+import prisma from "../config/db.js";
 
 // get all jobs
 export const getJobs = async (req, res) => {
-    try {
-        const jobs = await Job.find({ visible: true })
-            .populate({ path: 'companyId', select: '-password' })
+  try {
+    const jobs = await prisma.job.findMany({
+      where: {
+        visible: true,
+      },
+      include: {
+        company: {
+          select: {
+            id: true,
+            name: true,
+            email: true,
+            image: true,
+            createdAt: true,
+            updatedAt: true,
+          },
+        },
+      },
+    });
 
-        res.json({ success: true, jobs })
-
-    } catch (error) {
-        res.json({ success: false, message: error.message })
-    }
-}
+    res.json({ success: true, jobs });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({
+        success: false,
+        message: "An error occurred while fetching jobs.",
+      });
+  }
+};
 
 // get a single job by ID
 export const getJobById = async (req, res) => {
-    try {
-        const { id } = req.params
-        const job = await Job.findById(id)
-            .populate({
-                path: 'companyId',
-                select: '-password'
-            })
+  try {
+    const { id } = req.params;
 
-        if (!job) {
-            return res.json({
-                success: false,
-                message: 'Job not found'
-            })
+    const jobId = parseInt(id);
 
-        }
-
-        res.json({
-            success: true,
-            job
-        })
-    } catch (error) {
-        res.json({ success: false, message: error.message })
+    if (isNaN(jobId)) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid Job ID." });
     }
-}
+
+    const job = await prisma.job.findUnique({
+      where: {
+        id: jobId,
+      },
+      include: {
+        company: {
+          select: {
+            id: true,
+            name: true,
+            email: true,
+            image: true,
+            createdAt: true,
+            updatedAt: true,
+          },
+        },
+      },
+    });
+
+    if (!job) {
+      return res.status(404).json({
+        success: false,
+        message: "Job not found",
+      });
+    }
+
+    res.json({
+      success: true,
+      job,
+    });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({
+        success: false,
+        message: "An error occurred while fetching the job.",
+      });
+  }
+};
